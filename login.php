e68695e6b5aed30bc60736237449ca9ce89c2fd0
rehnxz01 <rehnxz01@gmail.com>
2026-06-24 10:54:55 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Admin</title>
+</head>
+<body>
+    <h2>Login Admin Toko Telur</h2>
+    <form action="proses_login.php" method="post">
+        <p>Username: <input type="text" name="user" required></p>
+        <p>Password: <input type="password" name="pass" required></p>
+        <button type="submit">Masuk</button>
+    </form>
+    <p><a href="index.php">Kembali ke Halaman Utama</a></p>
+</body>
+</html>
